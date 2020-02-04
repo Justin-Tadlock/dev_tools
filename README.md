aa22f0a0a07cd81a62c0a52acdb627625bfab0a0
JustinTadlock <justin.b.tadlock@gmail.com>
2020-02-03 19:17:52 -0800

    Initializing script

---
+++ README.md
@@ -0,0 +1,59 @@
+# Dev Tools
+This is a script that is used to install and configure dev tools on Windows.
+
+## What the Script Does
+This script will install a Windows package manager called `Chocolatey` that is used to install the listed software below:
+
+* Google Chrome
+* Firefox
+* 7zip
+* Git
+* Visual Studio Code
+* Visual Studio Community Edition 2013
+* Android Studio
+* Android SDK
+* OpenJDK
+* NodeJs-LTS
+* Yarn
+* DotNet Core
+* DotNet Core SDK
+* Sourcetree
+* Python
+* Python.PyPy
+
+Within Visual Studio Code, the following extensions are installed:
+
+* vscode-python
+* ms-python.python
+* pkief.material-icon-theme 
+* esbenp.prettier-vscode 
+* ms-vscode.powershell
+* dart-code.flutter
+* gmlewis-vscode.flutter-stylizer
+* redhat.java 
+* vscjava.vscode-java-test 
+* vscjava.vscode-java-pack 
+* msjsdiag.vscode-react-native 
+* jingkaizhao.vscode-redux-devtools
+
+Within NodeJS, the following packages are installed:
+
+* react-native-cli
+* create-react-app
+
+
+# Getting Started
+
+## Requirements
+* PowerShell
+* Admin access
+
+## Running the script
+1. Open an elevated PowerShell window (admin)
+2. Navigate to the directory containing the `init_dev_tools.ps1` script
+3. To run the script, execute the following command:
+
+    ```powershell -ExecutionPolicy bypass -File .\init_dev_tools.ps1```
+
+# Contributors
+* Justin Tadlock
---
+++ install_dev_tools.ps1
@@ -0,0 +1,45 @@
+# Install chocolatey for installing software
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Reset the command shell session
+refreshenv
+
+# Install tools
+choco install 7zip git -y
+
+# Install web browsers
+choco install googlechrome firefox -y
+
+# Install SDKs
+choco install openjdk nodejs-lts -y 
+
+# Install android dev tools
+choco install androidstudio android-sdk -y
+
+# Install package managers
+choco install yarn -y
+
+# Install dev tools
+choco install code visualstudiocommunity2013 sourcetree -y
+
+# Install NET 
+choco install dotnetcore dotnetcore-sdk -y 
+
+# Install python tools
+choco install python vscode-python python.pypy -y
+
+# Install react native
+choco install nodejs-lts -y
+npm install -g react-native-cli create-react-app -y
+
+# Install vscode extensions
+choco install code -y
+code --install-extension pkief.material-icon-theme esbenp.prettier-vscode ms-vscode.powershell
+# Install python extensions
+code --install-extention ms-python.python
+# Install Flutter extensions
+code --install-extension dart-code.flutter gmlewis-vscode.flutter-stylizer
+#Install Java extensions
+code --install-extension redhat.java vscjava.vscode-java-test vscjava.vscode-java-pack
+# Install react/react-native extensions
+code --install-extension msjsdiag.vscode-react-native jingkaizhao.vscode-redux-devtools
